7ed8d6c059463fa9636235b82535f7d82f1877a5
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-10 22:57:05 +1300

    got form book input mostly working, still need to fix form validation issues and style page

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let indexContainer = document.querySelector('.booksContainer')
+let newBook;
+let bookcount = 0;
+const myLibrary = [];
 
 function book(title, author, pages, read) {
     this.title = title;
@@ -11,9 +15,66 @@ function book(title, author, pages, read) {
         else {
             read = 'not read yet';
         }
-        return(`${title} by ${author}, ${pages} pages, ${read}.`);
+        //myLibrary.push(`${title} ${author} ${pages} ${read}`);
+        myLibrary.push(this);
+        
+        addLibraryToPage();
     }
 }
 
+function addBookToLibrary() {
+
+}
+let newArray = [];
+function addLibraryToPage() {
+    for (const books of myLibrary){
+        if(!newArray.includes(books) && newBookTime == true){
+        bookcount++;
+        newBook = document.createElement('div');
+        newBook.setAttribute('class', 'bookEntry');
+        newBook.setAttribute('id', (`book${bookcount}`));
+        newBook.textContent = (`${books.title} ${books.author} ${books.pages} ${books.read}`);
+        indexContainer.appendChild(newBook);
+        newArray.push(books);
+        newBookTime = false;
+        }
+    }
+}
+/*
 const theHobbit = new book('The Hobbit', 'J.R.R Tolken', 295, false);
-console.log(theHobbit.info());
+const theHobbit2 = new book('The Hobbit2', 'J.R.R Tolkennn', 1295, true);
+theHobbit.info();
+theHobbit2.info();
+const theHobbit3 = new book('The Hobbit3', 'J.R.R Tolkennn', 1295, true);
+theHobbit3.info();
+function bookInput() {
+    const newBook = new book(prompt("book:"));
+    myLibrary.push(newBook);
+    addLibraryToPage();
+}
+*/
+const bookForm = document.querySelector('.bookForm');
+const addBookButton = document.querySelector('.addBookButton');
+const submitButton = document.querySelector('.submitButton');
+const titleInput = document.getElementById('bookTitle');
+const authorInput = document.getElementById('author');
+const pageCount = document.getElementById('pageCount');
+const radioSec = document.querySelector('.pagesRadio');
+const radioY = document.getElementById("yes").value = "yes";
+const radioN = document.getElementById("no").value = "no";
+let newBookTime = false;
+addBookButton.addEventListener('click', ()=>{
+    newBookTime = true;
+    bookForm.style.display = 'flex';
+    bookForm.style.zIndex = '2';
+    submitButton.addEventListener('click', ()=> {
+        if(newBookTime == true){
+        const newBook = new book((titleInput.value), (authorInput.value), (pageCount.value), (radioSec.value));
+        bookForm.style.display = 'none';
+        bookForm.style.zIndex = '0';
+        myLibrary.push(newBook);
+        addLibraryToPage();
+        }
+    })  
+})
+    
