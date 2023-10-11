4d0ea33b75c9b4bf44c1eba22cace7eba3777a7d
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-11 19:28:13 +1300

    finished project for now, no storage but will come back later

--- javascript.js
+++ javascript.js
@@ -1,15 +1,14 @@
-let indexContainer = document.querySelector('.booksContainer')
+const indexContainer = document.querySelector('.booksContainer');
 let newBook;
 let bookcount = 0;
 const myLibrary = [];
-
-function book(title, author, pages, read) {
+function book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        if (read == 'read' || read == 'have read' || read == true){
+        if (read === 'read' || read == 'have read' || read == true){
             read = 'have read';
         }
         else {
@@ -17,13 +16,8 @@ function book(title, author, pages, read) {
         }
         //myLibrary.push(`${title} ${author} ${pages} ${read}`);
         myLibrary.push(this);
-        
         addLibraryToPage();
     }
-}
-
-function addBookToLibrary() {
-
 }
 let newArray = [];
 function addLibraryToPage() {
@@ -31,10 +25,72 @@ function addLibraryToPage() {
         if(!newArray.includes(books) && newBookTime == true){
         bookcount++;
         newBook = document.createElement('div');
+        let newBookTitle = document.createElement('h3');
+        let newBookAuthor = document.createElement('h3');
+        let newBookPages = document.createElement('h3');
+        let newBookTitleHeader = document.createElement('h3');
+        let newBookAuthorHeader = document.createElement('h3');
+        let newBookPagesHeader = document.createElement('h3');
+        let buttons = document.createElement('div');
+        let deleteButton = document.createElement('button');
+        let readButton = document.createElement('button');
+        deleteButton.textContent = 'DELETE'
+        buttons.setAttribute('class', 'buttonContainer');
+        deleteButton.setAttribute('id', (`button${bookcount}`));
+        deleteButton.setAttribute('class', (`delButton`));
+        newBookTitleHeader.textContent = 'Title:'
+        newBookAuthorHeader.textContent = 'Author:'
+        newBookPagesHeader.textContent = 'Pages:'
         newBook.setAttribute('class', 'bookEntry');
-        newBook.setAttribute('id', (`book${bookcount}`));
-        newBook.textContent = (`${books.title} ${books.author} ${books.pages} ${books.read}`);
+        newBook.setAttribute('id', (`${bookcount}`));
+        newBookTitle.textContent = (books.title);
+        newBookAuthor.textContent = (books.author);
+        newBookPages.textContent = (books.pages);
+        newBookTitle.style.fontWeight = ('400');
+        newBookAuthor.style.fontWeight = ('400');
+        newBookPages.style.fontWeight = ('400');
+        newBook.appendChild(newBookTitleHeader);
+        newBook.appendChild(newBookTitle);
+        newBook.appendChild(newBookAuthorHeader);
+        newBook.appendChild(newBookAuthor);
+        newBook.appendChild(newBookPagesHeader);
+        newBook.appendChild(newBookPages);
+        buttons.appendChild(deleteButton);
+        let readStatus;
+        if(radioSelection == 'Read'){
+            readButton.textContent = radioSelection;
+            readButton.style.backgroundColor = ('#69d2e7');
+            readStatus = 'read';
+        }
+        else{
+            readButton.textContent = radioSelection;
+            readButton.style.backgroundColor = ('#ffdf6d');
+            readStatus = 'notread';
+            readButton.style.fontSize = ('.45em')
+        }
+        buttons.appendChild(readButton)
+        newBook.appendChild(buttons);
+        readButton.addEventListener('click', ()=>{
+            if (readStatus == 'read'){
+                readButton.textContent = 'Not read';
+                readButton.style.backgroundColor = ('#ffdf6d');
+                readStatus = 'notread';
+            }
+            else{
+                readButton.textContent = 'Read';
+                readButton.style.backgroundColor = ('#69d2e7');
+                readStatus = 'read';
+                readButton.style.fontSize = ('.45em')
+            }
+        })
+        //newBook.textContent = (`${books.title} ${books.author} ${books.pages} ${books.read}`);
         indexContainer.appendChild(newBook);
+        document.getElementById(`button${bookcount}`).addEventListener('click', ()=> {
+            let deleteBook = (deleteButton.id).split("");
+            deleteBook = deleteBook[deleteBook.length - 1];
+            document.getElementById(deleteBook).remove();
+
+        })
         newArray.push(books);
         newBookTime = false;
         }
@@ -53,6 +109,7 @@ function bookInput() {
     addLibraryToPage();
 }
 */
+let radioSelection;
 const bookForm = document.querySelector('.bookForm');
 const addBookButton = document.querySelector('.addBookButton');
 const submitButton = document.querySelector('.submitButton');
@@ -60,14 +117,13 @@ const titleInput = document.getElementById('bookTitle');
 const authorInput = document.getElementById('author');
 const pageCount = document.getElementById('pageCount');
 const radioSec = document.querySelector('.pagesRadio');
-//const radioY = document.getElementById("yes").value = "no";
 let newBookTime = false;
 function handleForm(event) { 
     event.preventDefault(); 
     if(newBookTime == true){
-        let radioSelection;
+        
         if(document.getElementById('yes').checked) {
-            radioSelection = 'Have read';
+            radioSelection = 'Read';
         }
         else if (document.getElementById('no').checked) {
             radioSelection = 'Not read';
@@ -77,8 +133,7 @@ function handleForm(event) {
         bookForm.style.zIndex = '0';
         bookForm.reset();
         myLibrary.push(newBook);
-        addLibraryToPage();
-        
+        addLibraryToPage(); 
     } 
 }
 addBookButton.addEventListener('click', ()=>{
@@ -87,4 +142,4 @@ addBookButton.addEventListener('click', ()=>{
     bookForm.style.zIndex = '2';
     bookForm.addEventListener('submit', (handleForm))
 })
-    
+
